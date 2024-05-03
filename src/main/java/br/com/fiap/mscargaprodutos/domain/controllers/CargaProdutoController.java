f61dbe32bd2a6c56bebf7df9747fd15a435d26af
Matheus Lot Rizzo <matheus.l.rizzo@gmail.com>
2024-05-03 20:09:09 -0300

    Ajustes Revisão de Código

--- src/main/java/br/com/fiap/mscargaprodutos/domain/controllers/CargaProdutoController.java
+++ src/main/java/br/com/fiap/mscargaprodutos/domain/controllers/CargaProdutoController.java
@@ -1,17 +1,10 @@
 package br.com.fiap.mscargaprodutos.domain.controllers;
 
 import br.com.fiap.estrutura.swagger.annotations.ApiResponseSwaggerOk;
-import br.com.fiap.estrutura.utils.MessageErrorHandler;
 import br.com.fiap.estrutura.utils.SpringControllerUtils;
 import br.com.fiap.mscargaprodutos.domain.entities.JobLaunchRequest;
 import br.com.fiap.mscargaprodutos.domain.service.CargaProdutoService;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,15 +17,6 @@ public class CargaProdutoController {
     @Autowired
     private CargaProdutoService cargaProdutoService;
 
-    @Autowired
-    private JobLauncher jobLauncher;
-
-    @Autowired
-    private JobExplorer jobExplorer;
-
-    @Autowired
-    private ApplicationContext context;
-
     @PostMapping
     @ApiResponseSwaggerOk
     public ResponseEntity<?> carregarCargaProdutos(@RequestParam("arquivo") MultipartFile arquivo) {
@@ -42,16 +26,6 @@ public class CargaProdutoController {
     @PostMapping("/executar")
     @ApiResponseSwaggerOk
     public ResponseEntity<?> executarCargaProdutos(@RequestBody JobLaunchRequest request) {
-        try {
-            Job job = this.context.getBean(request.getName(), Job.class);
-
-            JobParameters jobParameters = new JobParametersBuilder(request.getJobParameters(), this.jobExplorer)
-                    .getNextJobParameters(job)
-                    .toJobParameters();
-
-            return ResponseEntity.ok().body(this.jobLauncher.run(job, jobParameters).getExitStatus());
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(MessageErrorHandler.create("Erro ao tentar executar a carga de produtos"));
-        }
+        return SpringControllerUtils.response(HttpStatus.OK, () -> cargaProdutoService.executarCargaProdutos(request));
     }
 }
--- src/main/java/br/com/fiap/mscargaprodutos/domain/service/CargaProdutoService.java
+++ src/main/java/br/com/fiap/mscargaprodutos/domain/service/CargaProdutoService.java
@@ -1,7 +1,16 @@
 package br.com.fiap.mscargaprodutos.domain.service;
 
 import br.com.fiap.estrutura.exception.BusinessException;
+import br.com.fiap.mscargaprodutos.domain.entities.JobLaunchRequest;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +26,15 @@ public class CargaProdutoService {
     @Value("${caminho.base}")
     private String caminhoBase;
 
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private JobExplorer jobExplorer;
+
+    @Autowired
+    private ApplicationContext context;
+
     public String receberArquivoCargaProdutos(MultipartFile arquivo) throws BusinessException {
         try{
             validaArquivo(arquivo);
@@ -26,19 +44,30 @@ public class CargaProdutoService {
             return "Arquivo salvo com sucesso";
         }catch (IOException e){
             throw new BusinessException("Erro ao salvar arquivo");
-        }catch (IllegalArgumentException e){
-            throw new BusinessException(e.getMessage());
         }
     }
 
-    private void validaArquivo(MultipartFile arquivo) {
+    public ExitStatus executarCargaProdutos(JobLaunchRequest request) throws BusinessException {
+        try{
+            Job job = this.context.getBean(request.getName(), Job.class);
+
+            JobParameters jobParameters = new JobParametersBuilder(request.getJobParameters(), this.jobExplorer)
+                    .getNextJobParameters(job)
+                    .toJobParameters();
+            return this.jobLauncher.run(job, jobParameters).getExitStatus();
+        }catch (Exception e) {
+            throw new BusinessException("Erro ao tentar executar a carga de produtos");
+        }
+    }
+
+    private void validaArquivo(MultipartFile arquivo) throws BusinessException {
         if (arquivo.isEmpty() || arquivo == null) {
-            throw new IllegalArgumentException("Arquivo não enviado");
+            throw new BusinessException("Arquivo não enviado");
         }
 
         String extensaoArquivo = extrairExtensao(arquivo.getOriginalFilename());
         if (!extensaoArquivo.equals("csv")) {
-            throw new IllegalArgumentException("Extensão do arquivo inválida");
+            throw new BusinessException("Extensão do arquivo inválida");
         }
     }
 
